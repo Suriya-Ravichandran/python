faf6c9c5911615e1d0c216426460639938619366
suriyaravichandran <rsuriya119@gmail.com>
2025-03-21 12:07:46 +0530

    update

--- batch3/mysql/insert.py
+++ batch3/mysql/insert.py
@@ -8,10 +8,10 @@ dbname="bank"
 conn=mysql.connector.connect(host=host,user=uname,password=passwd,database=dbname)
 cursor=conn.cursor()
 
-sql="SELECT * FROM USER"
+sql="INSERT INTO user (name,acc_type,balance)VALUES(%s,%s,%s)"
+val=("boo","current",2000)
 
-cursor.execute(sql)
-data=cursor.fetchall()
-
-for x in data:
-    print(x)
+cursor.execute(sql,val)
+conn.commit()
+print("data insert successfully")
+conn.close()
