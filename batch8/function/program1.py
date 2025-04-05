554fc1fdc60aa0b7fff6f2ec1a5757166b6c296a
suriyaravichandran <rsuriya119@gmail.com>
2025-04-05 14:40:26 +0530

    update

---
+++ batch8/function/program1.py
@@ -0,0 +1,22 @@
+# syntax of create function
+# def is keyword it is help to create a function
+
+# def function_name( arguments):
+    #  block of code
+    # return statement
+
+# Syntax of calling a function
+
+#function_name(arguments)
+
+
+def helloworld( addtext):
+    print("Hello World",addtext)
+
+helloworld("India")
+
+
+
+
+
+
