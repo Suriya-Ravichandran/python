8b43f5423ecd82789203c789bce2fa531bd39f77
suriyaravichandran <rsuriya119@gmail.com>
2025-05-08 16:46:05 +0530

    update mysql in batch10

---
+++ batch10/mysql/connect.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+host="localhost"
+user="root"
+password=""
+dbname="batch10"
+port=3307
+
+conn=mysql.connector.connect(host=host,user=user,password=password,database=dbname,port=port)
+
+if conn:
+    print("Connection sucessfully")
