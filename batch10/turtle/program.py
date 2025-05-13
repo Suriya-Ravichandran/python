6a69de1b756ae75778eac3e4b447f56b1e806461
suriyaravichandran <rsuriya119@gmail.com>
2025-05-13 16:02:45 +0530

    update

---
+++ batch10/turtle/program.py
@@ -0,0 +1,15 @@
+import turtle
+
+window=turtle.Screen()
+
+
+pen = turtle.Turtle()
+
+for x in range(15):
+    pen.forward(100)
+    pen.right(90)
+    pen.right(60)
+
+
+
+turtle.done()
---
+++ batch10/turtle/program2.py
@@ -0,0 +1,16 @@
+import turtle
+
+window=turtle.Screen()
+
+
+pen = turtle.Turtle()
+
+
+for x in range(4):
+    pen.circle(100)
+    pen.forward(100)
+    pen.right(90)
+
+
+
+turtle.done()
